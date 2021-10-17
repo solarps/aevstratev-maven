2a4033797e4d9dcdda1c19028278a628710d5024
Андрей <evstratevandrey@gmail.com>
2021-10-17 12:23:40 +0300

    HW11

--- src/main/java/HW11/TestLoginUtil.java
+++ src/main/java/HW11/TestLoginUtil.java
@@ -3,6 +3,6 @@ package HW11;
 public class TestLoginUtil {
     public static void main(String[] args) {
         LoginUtil.isUserAuthentic("baklajan","468s4afsas", "468s4afsas");
-        LoginUtil.isUserAuthentic("Valera","Sapojnik","Sapojnik");
+        LoginUtil.isUserAuthentic("Valera","Sapojnik","Sapojnik");//login doesn't exist in database
     }
 }
